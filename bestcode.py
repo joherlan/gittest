8ff047656409fe77ea68a1946eeb9740fa5fe568
joherlan <35494021+joherlan@users.noreply.github.com>
2021-09-21 16:06:22 +0200

    Update bestcode.py
    Added code to x

--- bestcode.py
+++ bestcode.py
@@ -3,3 +3,11 @@ print ("hej world för fan")
 print ("Detta var fan en uppdatering")
 
 print("Nu ska vi se om detta kommer till min datta")
+
+print("OKej najs det är till min datta dags för mer häftig kod!")
+
+
+x = "hej"
+
+for i in range (10):
+    print(x)
